309a7c9364327cc8f2341954c3bd4c0a0d783a17
Bryan Keener <bkeener7@gmail.com>
2022-08-02 13:38:40 -0600

    Added puts statements

--- variables.rb
+++ variables.rb
@@ -1,7 +1,12 @@
 animal_shelter = "Lubbock County Shelter 123"
+animal_name = "Sage"
 animal_age = 2
 animal_weight = 55
 adoption_fee = 20.00
 neck_size = 31.2
 vaccinated = true
 fixed = true
+animal_shelter = "Amarillo County Shelter 123"
+puts "#{animal_name} is #{animal_age} years old."
+puts "#{animal_name} is currently located at #{animal_shelter}."
+puts "#{animal_name} would love to be adopted today!"
